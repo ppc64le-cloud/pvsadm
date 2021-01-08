ca85a63283f5718951457d157c07fabcd32a752b
Manjunath A Kumatagi <mkumatag@in.ibm.com>
2021-01-08 17:44:35 +0530

    hack scripts

---
+++ hack/update-copyright.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Copyright 2021 IBM Corp
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")/..
+
+source "${ROOT_DIR}/hack/lib/init.sh"
+
+echo "Update copyrights!"
+
+command -v addlicense >/dev/null 2>&1 || { echo >&2 "Require addlicense to run this script, please install by \"go get -u github.com/google/addlicense\" and try again"; exit 1; }
+
+pushd ${ROOT_DIR}
+
+find_files() {
+  find . -not \( \
+      \( \
+        -wholename './.git' \
+        -o -wholename '*/vendor/*' \
+      \) -prune \
+    \) -name '*.go' -o -name "Dockerfile*" -o -name '*.sh'
+}
+
+diff=$(find_files | xargs addlicense -c "IBM Corp" -l apache 2>&1) || true
+
+popd
