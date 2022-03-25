28d0b49ed58772b38c34b15a126d544fdc2b330f
Prajyot-Parab <prajyot.parab2@ibm.com>
2022-03-25 15:41:04 +0530

    Add image import job layer to import process
    Signed-off-by: Prajyot-Parab <prajyot.parab2@ibm.com>

--- pkg/client/image/image.go
+++ pkg/client/image/image.go
@@ -16,22 +16,20 @@ package image
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"regexp"
 	"time"
 
 	"github.com/IBM-Cloud/power-go-client/clients/instance"
 	"github.com/IBM-Cloud/power-go-client/ibmpisession"
-	"github.com/IBM-Cloud/power-go-client/power/client/p_cloud_images"
 	"github.com/IBM-Cloud/power-go-client/power/models"
 	"github.com/ppc64le-cloud/pvsadm/pkg"
-	"k8s.io/klog/v2"
 )
 
 type Client struct {
 	session    *ibmpisession.IBMPISession
 	client     *instance.IBMPIImageClient
+	jobclient  *instance.IBMPIJobClient
 	instanceID string
 }
 
@@ -41,6 +39,7 @@ func NewClient(sess *ibmpisession.IBMPISession, powerinstanceid string) *Client 
 		instanceID: powerinstanceid,
 	}
 	c.client = instance.NewIBMPIImageClient(context.Background(), sess, powerinstanceid)
+	c.jobclient = instance.NewIBMPIJobClient(context.Background(), sess, powerinstanceid)
 	return c
 }
 
@@ -56,37 +55,29 @@ func (c *Client) Delete(id string) error {
 	return c.client.Delete(id)
 }
 
+func (c *Client) CreateCosImage(body models.CreateCosImageImportJob) (*models.JobReference, error) {
+	return c.client.CreateCosImage(&body)
+}
+
 //func ImportImage imports image from S3 Instance
-func (c *Client) ImportImage(instanceID, imageName, s3Filename, region, accessKey, secretKey, bucketName, storageType string) (*models.Image, error) {
-	var source = "url"
-	var body = models.CreateImage{
-		ImageName:     imageName,
-		ImageFilename: s3Filename,
-		Region:        region,
+func (c *Client) ImportImage(imageName, s3Filename, region, accessKey, secretKey, bucketName, storageType string) (*models.JobReference, error) {
+
+	var body = models.CreateCosImageImportJob{
+		ImageName:     &imageName,
+		ImageFilename: &s3Filename,
+		Region:        &region,
 		AccessKey:     accessKey,
 		SecretKey:     secretKey,
-		BucketName:    bucketName,
-		DiskType:      storageType,
-		Source:        &source,
+		BucketName:    &bucketName,
+		StorageType:   storageType,
 	}
 
-	params := p_cloud_images.NewPcloudCloudinstancesImagesPostParamsWithTimeout(pkg.TIMEOUT).WithCloudInstanceID(instanceID).WithBody(&body)
-	resp1, resp2, err := c.session.Power.PCloudImages.PcloudCloudinstancesImagesPost(params, c.session.AuthInfo(c.instanceID))
-
+	jobRef, err := c.CreateCosImage(body)
 	if err != nil {
 		return nil, err
 	}
 
-	if resp1 != nil {
-		klog.Errorf("Failed to intiate the import job")
-		return nil, errors.New("failed to initiate the import job")
-	}
-
-	if resp2.Payload.State == "queued" {
-		klog.Infof("Post is successful %s", *resp2.Payload.ImageID)
-	}
-
-	return resp2.Payload, nil
+	return jobRef, nil
 }
 
 func (c *Client) GetAllPurgeable(before, since time.Duration, expr string) ([]*models.ImageReference, error) {
@@ -108,4 +99,17 @@ func (c *Client) GetAllPurgeable(before, since time.Duration, expr string) ([]*m
 		candidates = append(candidates, image)
 	}
 	return candidates, nil
+}
+
+func (c *Client) GetImageByName(imageName string) (*models.ImageReference, error) {
+	images, err := c.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	for _, img := range images.Images {
+		if *img.Name == imageName {
+			return img, nil
+		}
+	}
+	return nil, nil
 }
